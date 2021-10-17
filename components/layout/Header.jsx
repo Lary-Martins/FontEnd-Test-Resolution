913f3f43e228e0f6baafdc8de32209447e3594bd
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-10-17 16:33:14 -0300

    change - troca referencia tag para className

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -5,11 +5,13 @@ import logo from '../../public/logo.png'
 import '../../styles/Header.css';
 
 export default function TuriviusHeader() {
-    return <div className="turivius-header">
+    return (
+      <div className="turivius-header">
         <Image 
+         className="img"
          src={ logo }
          alt="Turivius logo"
-         className="img"
         />
-    </div>
+      </div>
+    )
 }
