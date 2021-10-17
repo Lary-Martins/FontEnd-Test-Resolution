cbe53a57216ce6a48ff6adca5d606c7068ae20d8
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-10-17 16:53:12 -0300

    feat - implementa prop "entities" ao componente "Card"

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Panel } from 'rsuite';
 import '../../styles/Card.css';
 
-export default function Card({ content }) {
+export default function Card({ card, entities }) {
+  console.log({ card, entities })
+
   return (
     <Panel className="turivius-card"
       shaded
       bordered
     >
-      {content.map(c => 
+      {card.content.map(c => 
        (<p key={c.title}>
          <b>{c.title}:</b> {c.content}
        </p>)
