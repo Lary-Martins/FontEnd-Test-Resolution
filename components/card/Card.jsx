e744d16b3bb1bdf10f3c44970d3f3a36b45384b7
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-10-17 21:30:25 -0300

    feat - implementa button "Ver mais"

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Panel } from 'rsuite';
+import React, { useState } from 'react';
+import { Panel, Button, ButtonToolbar } from 'rsuite';
 import '../../styles/Card.css';
 
 export default function Card({ card, entities }) {
   console.log({ card, entities })
+  const [showMore, setShowMore] = useState(false);
 
   const delimitsNumberOfWords = (text) => {
     const arryOfWords = text.split(' ', 50);
@@ -12,17 +13,32 @@ export default function Card({ card, entities }) {
     return delimetedText;
   }
 
+  const handleClickShowMore = () => {
+    setShowMore(!showMore);
+  }
+
   return (
-    <Panel className="turivius-card"
+    <Panel
+      className="turivius-card"
       shaded
       bordered
     >
       {card.content.map(c => 
        (<p key={c.title}>
          <b>{c.title}:</b>
-         { `${delimitsNumberOfWords(c.content)} ...`}
+         { showMore ? c.content : `${delimitsNumberOfWords(c.content)} ...`}
        </p>)
       )}
+      <ButtonToolbar
+       className="btn">
+        <Button
+          className="btn-show"
+          appearance="ghost"
+          onClick={ handleClickShowMore }
+          >
+            Ver mais
+        </Button>
+      </ButtonToolbar>
     </Panel>
 )
 }
